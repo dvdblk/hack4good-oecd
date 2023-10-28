03bc4f0dcb913c04546eddc1e86296cf5ae6b885
dvdblk <git@dvdblk.com>
2023-10-28 19:57:31 +0200

    chore: add some preprocessing methods

--- experiments/alex/pdf2text/preprocessing/methods.py
+++ experiments/alex/pdf2text/preprocessing/methods.py
@@ -1,4 +1,6 @@
 """Preprocessing methods (steps) for pdf elements are defined in this file."""
+import re
+
 from typing import Optional
 from pdf2text.preprocessing.decorator import register_preprocessor
 from pdf2text.models import PDFContentSource, PDFContentScope
@@ -11,14 +13,41 @@ def remove_newlines(line: str) -> Optional[str]:
 
 
 @register_preprocessor(PDFContentSource.TEXT, PDFContentScope.LINE)
+def remove_unnecessary_whitespace(line: str) -> Optional[str]:
+    """Removes unnecessary whitespace"""
+    return re.sub(r"\s+", " ", line)
+
+
+@register_preprocessor(PDFContentSource.TEXT, PDFContentScope.LINE)
 def remove_empty_lines(line: str) -> Optional[str]:
     """Removes empty lines"""
-    if line.strip() == "":
+    whitespace_removed = re.sub(r"\s", "", line)
+    if whitespace_removed == "":
         # Return None to indicate that this line should be removed
         return None
     else:
         # Identity
         return line
+
+
+# @register_preprocessor(PDFContentSource.TEXT, PDFContentScope.PARAGRAPH)
+def remove_numbers(paragraph: str) -> Optional[str]:
+    """Removes numbers including decimal points or surrounded by brackets"""
+    return re.sub(r"\(?\d+\.?\d*\)?", "", paragraph)
+
+
+@register_preprocessor(PDFContentSource.TEXT, PDFContentScope.PARAGRAPH)
+def remove_index_dots(line: str) -> Optional[str]:
+    """Removes index dots inside the line"""
+    return re.sub(r"\s*(\.\ |\.){4,}\s*", "", line)
+
+
+@register_preprocessor(PDFContentSource.TEXT, PDFContentScope.PARAGRAPH)
+def remove_numeric_lines(paragraph: str) -> Optional[str]:
+    if paragraph.isnumeric():
+        return None
+    else:
+        return paragraph
 
 
 @register_preprocessor(PDFContentSource.TEXT, PDFContentScope.PARAGRAPH)
